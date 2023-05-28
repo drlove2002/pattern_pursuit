0d17f3fff4267d699c08ac1cad32f65a718e069f
Sudip Roy <sudiproy20yo@gmail.com>
2023-05-28 23:23:28 +0530

    feat(PopUp Window): help message for help button

--- frontend/src/app/js/script.js
+++ frontend/src/app/js/script.js
@@ -10,7 +10,9 @@ $(window).on("load", function () {
     // Set bt margin-top and font-size
 });
 
+
 $(document).ready(function () {
+
     const nav = $(".nav-container");
 
     if (nav.length) {
@@ -30,5 +32,51 @@ $(document).ready(function () {
             });
         }
     }
+
+    // Pop up window
+    const popup = $("#popup-window");
+
+    // Make a function to open popup window with contains
+    $(".button").on('click', (e) => {
+        // Return case
+        if (popup.hasClass("is-active"))
+            return;
+
+        var buttonId = e.currentTarget.id;
+        if (buttonId == "help")
+            var popupContent =
+                '<div id="rules">' +
+                '<h2>How to play?</h1>' +
+                '<p>Welcome to "Guess the Coin Toss"! In this game, you will be prompted to choose between' +
+                '"heads" or "tails". The bot will then attempt to guess your choice before it is revealed. If' +
+                'the bot guesses correctly, you will lose $100 and the bot will earn $100. If the bot guesses' +
+                'incorrectly, you will earn $100 and the bot will lose $100. The challenge is to outsmart the' +
+                'computer by being as random as possible to earn more money. Have fun and good luck!</p>' +
+                '</div>';
+        else
+            var popupContent = "";
+
+        popup.addClass("is-active");
+        popup.find(".popup-content").html(popupContent);
+
+        // Stop interactivity with the background when popup is open, only allow to close popup
+        nav.addClass("disable-interaction");
+        $(".content").addClass("disable-interaction");
+    });
+
+    // Make a function to close popup window
+    popup.find(".popup-close").on("click", () => {
+        // Return case
+        if (!popup.hasClass("is-active"))
+            return;
+
+        popup.removeClass("is-active");
+        popup.find(".popup-content").html("");
+
+        // Allow interactivity with the background when popup is close
+        nav.removeClass("disable-interaction");
+        $(".content").removeClass("disable-interaction");
+    });
+
 });
 
