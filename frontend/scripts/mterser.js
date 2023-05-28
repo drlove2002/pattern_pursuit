e59adf1dc6e657c1db340efcfda5341d2d507d31
Sudip Roy <sudiproy20yo@gmail.com>
2023-05-28 23:31:46 +0530

    chore: Update some dependency

--- frontend/scripts/mterser.js
+++ frontend/scripts/mterser.js
@@ -1,9 +1,16 @@
-const { readdirSync } = require("fs");
+const { readdirSync, mkdirSync } = require("fs");
 const { exec } = require("child_process");
+const targetFolder = "./docs/js";
+
+// Check if the target folder exists, create it if not
+if (!readdirSync("./docs").includes("js")) {
+  mkdirSync(targetFolder);
+}
+
 readdirSync("./src/app/js").forEach((file) => {
   if (file.endsWith(".js")) {
     exec(
-      `terser ./src/app/js/${file} --compress --mangle -o ./docs/js/${file}`,
+      `terser ./src/app/js/${file} --compress --mangle -o ${targetFolder}/${file}`,
       (e) => {
         if (e !== null) {
           console.log(`exec error: ${e}`);
