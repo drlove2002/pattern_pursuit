cec3ee64ce5a692aacb3fa4725a716634e4c6755
Sudip Roy <sudiproy20yo@gmail.com>
2023-05-28 23:47:03 +0530

    feat(Server): Added basic actix server and deps

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::middleware::Logger;
+use actix_web::{get, App, HttpServer};
+use actix_web::{HttpResponse, Responder};
+
+#[get("/api/healthchecker")]
+async fn health_checker_handler() -> impl Responder {
+    const MESSAGE: &str = "I'm up and running!";
+
+    HttpResponse::Ok().json(serde_json::json!({"status": "success", "message": MESSAGE}))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    if std::env::var_os("RUST_LOG").is_none() {
+        std::env::set_var("RUST_LOG", "actix_web=info");
+    }
+    env_logger::init();
+
+    println!("🚀 Server started successfully");
+
+    HttpServer::new(move || {
+        App::new()
+            .service(health_checker_handler)
+            .wrap(Logger::default())
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
